0153a1e9412636181d58f7b1a0ecf8807f3d394b
Jerome Truong <jerometruong@gmail.com>
2013-12-17 07:03:58 -0600

    lua function locator

--- perso-lua.el
+++ perso-lua.el
@@ -1,18 +1,31 @@
+;;;;;;;;;;;;;
+;; require ;;
+;;;;;;;;;;;;;
+(require 'lua-mode)
+
 ;;;;;;;;;;;;;;
 ;; settings ;;
 ;;;;;;;;;;;;;;
-(setq perso/lua/function-regexp "function \\(?1:.*\\)([^)])")
+(setq perso/lua/regexp/function "function \\(?1:.*\\)([^)])")
 
 ;;;;;;;;;;;;;;;
 ;; functions ;;
 ;;;;;;;;;;;;;;;
-(defun perso/lua/prev-function ()
-  )
+(defun perso/lua/previous-function ()
+  ;; TODO macro these previous/next helpers
+  (interactive)
+  (re-search-backward perso/lua/regexp/function nil t)
+  (goto-char (match-beginning 1)))
+
+(defun perso/lua/next-function ()
+  (interactive)
+  (re-search-forward perso/lua/regexp/function nil t)
+  (goto-char (match-beginning 1)))
 
 ;;;;;;;;;;;;;;;;;
 ;; keybindings ;;
 ;;;;;;;;;;;;;;;;;
-(define-key lua-mode-map (kbd "M-p") 'perso/lua/prev-function)
-(define-key lua-mode-map (kbd "M-n") 'perso/lua/prev-function)
+(define-key lua-mode-map (kbd "M-p") 'perso/lua/previous-function)
+(define-key lua-mode-map (kbd "M-n") 'perso/lua/next-function)
 
 (provide 'perso-lua)
