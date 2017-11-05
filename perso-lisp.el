f656af72ea51de257f619080204d8a8ff93fd4a9
Jerome Truong <jerometruong@gmail.com>
2017-11-05 09:15:05 -0600

    removed lisp indent function

--- perso-lisp.el
+++ perso-lisp.el
@@ -6,13 +6,17 @@
 ;;;;;;;;;;;;;;
 ;; settings ;;
 ;;;;;;;;;;;;;;
-(setq lisp-indent-function 'common-lisp-indent-function)
-(eval-after-load "cl-indent"
-  '(progn
-    (put 'cl-flet      'common-lisp-indent-function (get 'flet 'common-lisp-indent-function))
-    (put 'css-lite:css 'common-lisp-indent-function '(&rest (&whole 2 &rest nil)))
-    (put 'create-folder-dispatcher-and-handler 'common-lisp-indent-function '(&rest (&whole 2 &rest nil)))
-    (put 'ps* 'common-lisp-indent-function '(&rest 2))))
+;;; Slime
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-contribs '(slime-fancy slime-cl-indent))
+
+;; (eval-after-load "cl-indent"
+;;   '(progn
+;;     (put 'cl-flet      'common-lisp-indent-function (get 'flet 'common-lisp-indent-function))
+;;     (put 'css-lite:css 'common-lisp-indent-function '(&rest (&whole 2 &rest nil)))
+;;     (put 'create-folder-dispatcher-and-handler 'common-lisp-indent-function '(&rest (&whole 2 &rest nil)))
+;;     (put 'ps* 'common-lisp-indent-function '(&rest 2))))
 
 (add-hook 'lisp-mode-hook
           '(lambda ()
