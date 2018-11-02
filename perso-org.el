1453994ff470fd9fe7885007745e368acb3b3676
Jerome Truong <jerometruong@gmail.com>
2018-11-02 17:02:55 -0500

    org changes
    - pomidor replaces org timer
    
    - `auto-fill-mode` on by default

--- perso-org.el
+++ perso-org.el
@@ -1,16 +1,14 @@
 (require 'package)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
-(setq org-timer-default-timer 25)
-(add-hook 'org-clock-in-hook (lambda ()
-                               (org-timer-set-timer)))
-
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((shell . t)
    (emacs-lisp . t)))
 
 (add-hook 'org-mode-hook
-          (lambda () (set-fill-column 60)))
+          (lambda ()
+            (set-fill-column 60)
+            (auto-fill-mode 1)))
 
 (provide 'perso-org)
